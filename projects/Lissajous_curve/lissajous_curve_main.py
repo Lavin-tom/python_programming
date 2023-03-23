dd34b816418b0ffd1ef3d3da1b7af016d138b403
Lavin-tom <lavintom007@gmail.com>
2023-03-23 21:46:19 +0530

    modify lissajous-curve project

--- projects/Lissajous_curve/lissajous_curve_main.py
+++ projects/Lissajous_curve/lissajous_curve_main.py
@@ -3,8 +3,10 @@ import math
 
 # Initialize pygame
 pygame.init()
+pygame.font.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Lissajous Curves")
+font = pygame.font.SysFont('Arial', 15)
 clock = pygame.time.Clock()
 
 # Define some colors
@@ -37,22 +39,24 @@ c23 = pygame.Color(255,0,128)
 c24 = pygame.Color(255,0,64)
 c25 = pygame.Color(255,0,0)
 
-colors = []
-for r in range(255, -1, -64):
-    for b in range(0, 256, 32):
-        for g in range(0, 256, 32):
-            colors.append(pygame.Color(r, g, b))
-            
+
 #color = [violet,indigo,blue,green,yellow,orange,red]
 color = [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18,
          c19,c20,c21,c22,c23,c24,c25]
+
+deltas = [0,math.pi/4,math.pi/2,3**math.pi/4,math.pi]
+
+delta_value =['zero','pi/4','pi/2','3pi/4','pi']
+
 # Define some parameters
 A = 200 # Scaling factor for x
 B = 200 # Scaling factor for y
 a = 5 # Frequency for x
 b = 4 # Frequency for y
-delta = math.pi / 2 # Phase angle
+d = 0 # set the delta value as 0 from deltas 
 
+#delta = math.pi / 2 # Phase angle
+delta = deltas[d]
 # Define some variables
 t = 0 # Time parameter
 dt = 0.001 # Time increment
@@ -62,6 +66,11 @@ screen.fill(BLACK)
 # for color change
 i = 0
 delay = 0
+
+def displayValues(a,b,delta):
+    text = font.render(f'A:{A} B:{B} ω_a:{a} ω_b:{b} Phase:{delta}', True, color[i])
+    screen.fill(BLACK, (300,570, 300,20))
+    screen.blit(text, (300,570))
 
 # Main loop
 running = True
@@ -74,24 +83,63 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             elif event.key== pygame.K_DOWN:
-                a -= 1
-                screen.fill(BLACK)
-                print(a)
+                if a>1:
+                    a -= 1
+                    screen.fill(BLACK)
                 
             elif event.key == pygame.K_UP:
-                a += 1
-                screen.fill(BLACK)
-                print(a)
+                if a<5:
+                    a += 1
+                    screen.fill(BLACK) 
+
+            elif event.key == pygame.K_LEFT:
+               if b>1:
+                    b -= 1
+                    screen.fill(BLACK)
+                
+            elif event.key == pygame.K_RIGHT:
+                if b<5:
+                    b += 1
+                    screen.fill(BLACK) 
+
+            elif event.key == pygame.K_s:
+               if d>0:
+                    d -= 1
+                    screen.fill(BLACK)
+                
+            elif event.key == pygame.K_w:
+                if d<4:
+                    d += 1
+                    screen.fill(BLACK)
+            
+            elif event.key == pygame.K_a:
+                if A>0:
+                    A -= 10
+                    screen.fill(BLACK)
+            
+            elif event.key == pygame.K_q:
+                if A<250:
+                    A += 10
+                    screen.fill(BLACK)
+
+            elif event.key == pygame.K_d:
+                if A>0:
+                    B -= 10
+                    screen.fill(BLACK)
+            
+            elif event.key == pygame.K_e:
+                if A<250:
+                    B += 10
+                    screen.fill(BLACK)
+
     
-
-
     # Calculate x and y coordinates of the point on the curve
-    x = A * math.sin(a * t + delta) + 400 # Add 400 to center on screen
+    x = A * math.sin(a * t + deltas[d]) + 400 # Add 400 to center on screen
     y = B * math.sin(b * t) + 300 # Add 300 to center on screen
 
     # Draw the point as a white circle with radius 1 pixels 
     pygame.draw.circle(screen, color[i], (int(x), int(y)), 1)
-
+    displayValues(a,b,delta_value[d]) 
     # Increment time parameter 
     t += dt
 
@@ -101,6 +149,7 @@ while running:
     # Limit frame rate 
     clock.tick(500)
 
+    #for color changing 
     delay+=1
     if delay == 200:
         i+=1
